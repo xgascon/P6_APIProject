230a7f1ff321a69b91662f26e77cc2913e79a860
xgascon <65670213+xgascon@users.noreply.github.com>
2020-11-06 20:09:29 +0100

    first commit prod route POST

---
+++ routes/stuff.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.get('/', auth, stuffCtrl.getAllSauce);
+router.post('/', auth, multer, stuffCtrl.createSauce);
+router.get('/:id', auth, stuffCtrl.getOneSauce);
+router.put('/:id', auth, multer, stuffCtrl.modifySauce);
+router.delete('/:id', auth, stuffCtrl.deleteSauce);
+
+module.exports = router;
